a329002366ca41100a19955295e2b6724e612d28
Giannis Melidis <giannismelidis@olery.com>
2013-06-19 10:25:49 +0200

    Changes so that it works like the base one

--- lib/opener/pos_taggers/en.rb
+++ lib/opener/pos_taggers/en.rb
@@ -1,31 +1,75 @@
+require 'open3'
 require_relative 'en/version'
 
 module Opener
   module POSTaggers
+    ##
+    # The POS tagger that supports English and Spanish.
+    #
+    # @!attribute [r] args
+    #  @return [Array]
+    # @!attribute [r] options
+    #  @return [Hash]
+    # @!attribute [r] option_parser
+    #  @return [OptionParser]
+    #
     class EN
-      attr_reader :kernel, :lib
+      attr_reader :args, :options
 
+      ##
+      # @param [Hash] options
+      #
+      # @option options [Array] :args The commandline arguments to pass to the
+      #  underlying Python script.
+      #
+      def initialize(options = {})
+        @args          = options.delete(:args) || []
+        @options       = options
+      end
+
+      ##
+      # Builds the command used to execute the kernel.
+      #
+      # @return [String]
+      #
       def command(opts=[])
         "java -jar #{kernel} -l en #{opts.join(' ')}"
       end
-      
-      def run(opts=ARGV)
-        `#{command(opts)}`
+
+      ##
+      # Runs the command and returns the output of STDOUT, STDERR and the
+      # process information.
+      #
+      # @param [String] input The input to tag.
+      # @return [Array]
+      #
+      def run(input)
+        return Open3.capture3(command, :stdin_data => input)
       end
-      
+
       protected
-      
+
+      ##
+      # @return [String]
+      #
       def core_dir
         File.expand_path("../../../core", File.dirname(__FILE__))
-      end
-      
+      end  
+
+      ##
+      # @return [String]
+      #
       def kernel
         core_dir+'/target/ehu-pos-1.0.jar'
-      end      
-      
-    end
-  end
-end
+      end 
+    end # EN
+
+    class ES < EN
+    end # ES
+
+  end # POSTaggers
+end # Opener
+
 
 
 
