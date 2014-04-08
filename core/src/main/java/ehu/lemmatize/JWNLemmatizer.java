a199fd0c575fd40e87b46563ba2d60ad76fe45db
Rodrigo Agerri <agerri.rodrigo@gmail.com>
2014-04-08 14:49:23 +0200

    removing traces

--- core/src/main/java/ehu/lemmatize/JWNLemmatizer.java
+++ core/src/main/java/ehu/lemmatize/JWNLemmatizer.java
@@ -1,19 +1,3 @@
-/*
- *Copyright 2013 Rodrigo Agerri
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- */
-
 package ehu.lemmatize;
 
 import ehu.pos.Resources;
@@ -43,7 +27,6 @@ import net.didion.jwnl.princeton.data.PrincetonWN17FileDictionaryElementFactory;
 import net.didion.jwnl.princeton.file.PrincetonRandomAccessDictionaryFile;
 
 /**
- * @author ragerri
  * 
  *         Class to obtain English lemmas using JWNL API.
  * 
--- core/src/main/java/ehu/pos/POS.java
+++ core/src/main/java/ehu/pos/POS.java
@@ -1,18 +1,3 @@
-/*Copyright 2013 Rodrigo Agerri
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- */
-
 package ehu.pos;
 
 import java.io.IOException;
@@ -23,10 +8,6 @@ import opennlp.tools.postag.POSTaggerME;
 
 /**
  * Simple POS tagging module based on Apache OpenNLP.
- * 
- * Models trained by IXA NLP Group.
- * 
- * @author ragerri 2012/11/30
  * 
  * 
  * CHANGES: Added a constructor to leverage the static model loading
