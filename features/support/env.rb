4c85cacdafa2d7561c756435a4a24e931190bff1
Yorick Peterse <yorickpeterse@olery.com>
2014-06-12 13:36:49 +0200

    Use RSpec 3.0.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,16 @@
 require_relative '../../lib/opener/pos_taggers/en'
+require 'rspec'
 
 def kernel
   Opener::POSTaggers::EN.new
 end
 
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
